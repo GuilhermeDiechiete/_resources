18da91ee46e4edd1dae7ee2465856298483b6b74
Guilherme Diechiete <132406389+GuilhermeDiechiete@users.noreply.github.com>
2024-03-02 23:14:25 -0300

    functions python

---
+++ python/Cap6/Listas.py
@@ -0,0 +1,14 @@
+# Listas são um tipo de variável que permite o armazenamento de vários valores, acessados por um índice.
+# Uma lista pode conter zero ou mais elementos de um mesmo tipo ou de tipo diversos, podendo inclusive conter outras listas.
+
+L = [] # Lista vazia
+print(L)
+
+Lista = [15, 8, 9]
+print(Lista)
+
+
+print(Lista[0]) # buscar um valor pelo indice
+
+Lista[0] = 100 # alterar um valor
+print(Lista)
