931b4e13c422b50890c6c0261e032acf2fe416d3
Guilherme Diechiete <132406389+GuilhermeDiechiete@users.noreply.github.com>
2024-03-06 21:11:25 -0300

    Update cortar-slice.py

--- python/Cap5/cortar-slice.py
+++ python/Cap5/cortar-slice.py
@@ -1,4 +1,19 @@
-name = "Guilherme"
-#index  012345678
 
-print(name[4])
+# Exemplo 1: Obtendo parte de uma lista
+lista = [ 1,2,3,4,5,6,7,8,9,10]
+parte_da_lista = lista[1:4]
+print(parte_da_lista)
+
+# Exemplo 2: Obtendo uma parte de uma string
+texto = "Olá, Guilherme"
+parte_do_texto = texto[4:9]
+print(parte_do_texto)
+
+# Exemplo 3: Passo negativo para inverter a lista ou string
+lista = [ 1,2,3,4,5,6,7,8,9,10]
+lista_invertida = lista[::-1]
+print(lista_invertida)
+
+testo = 'Python'
+texto_invertido = texto[::1]
+print(textp_invertido)
